e6b8f4a2c9ad1cae120c0e27e05c762a1eeccbd6
Ryan Hogan <Ryanoman2011@Hotmail.com>
2024-03-26 19:16:34 +0000

    Basics of a main menu started.

---
+++ Assets/Scripts/UI/Buttons.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Buttons : MonoBehaviour
+{
+    // Loads the relevent scene when the button is pressed.
+
+    public void PlayButton() {
+        Debug.Log("Play button pressed. ");
+        SceneManager.LoadScene("Level 01");
+        Debug.Log("Loaded scene: Level 01 ");
+    }
+
+    public void TutorialButton() {
+        Debug.Log("Tutorial button pressed. ");
+        SceneManager.LoadScene("Tutorial");
+        Debug.Log("Loaded scene: Tutorial ");
+    }
+
+    public void SettingsButton() {
+        Debug.Log("Settings button pressed. ");
+        SceneManager.LoadScene("Preferences");
+        Debug.Log("Loaded scene: Preferences ");
+    }
+}
