dea02afe5d7a617a96b93eadf3b205c51bd0c3e9
Ryan Hogan <Ryanoman2011@Hotmail.com>
2024-04-09 11:56:41 +0100

    Removed a comment.

--- Assets/Scripts/Player/Projectile.cs
+++ Assets/Scripts/Player/Projectile.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class Projectile : MonoBehaviour {
   public float projectileLifeTime = 1f;
 
-  // Start is called before the first frame update.
   void Start() {
     // Destroy the projectile after a certain amount of time.
     Destroy(gameObject, projectileLifeTime);
