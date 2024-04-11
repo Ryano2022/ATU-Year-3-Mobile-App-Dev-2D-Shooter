cb3ec73cc1ddde451462dc619ec10034fab0b128
Ryan Hogan <101425132+Ryano2022@users.noreply.github.com>
2024-04-11 18:02:42 +0100

    Added sprites for the weapon and ammo. Added functionality to the weapon.

--- Assets/Scripts/Player/PlayerWeapon.cs
+++ Assets/Scripts/Player/PlayerWeapon.cs
@@ -1,26 +1,70 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerWeapon : MonoBehaviour
 {
-    [SerializeField] private GameObject projectilePrefab; // GameObject Prefab to fire from player weapon.
-    private Transform firePoint;                          // Where to spawn projectile from.
-    private Rigidbody2D rb;                               // Rigidbody2D component of the player.
+    [SerializeField] private InputActionReference attack;
+    [SerializeField] private Projectile projectileParent;
+    [SerializeField] private float projectileSpeed = 20f;
+    [SerializeField] private float fireRate = 0.1f;
+    private Coroutine firingCoroutine;
+    private int facingDirection = 1;
 
-    void Start() {
-        rb = GetComponent<Rigidbody2D>();
-    }
+    // Coroutine returns an IEnumerator type.
+    private IEnumerator FireCoroutine() {
+        while (true) {
+            // If the player is looking right, instantiate the bullet and set its velocity to move right.
+            if (Input.GetAxis("Horizontal") > 0) {
+                facingDirection = 1;
+            }
 
-    void Update() {
-        // Fire a bullet when the player presses the Fire1 button.
-        if (Input.GetButtonDown("Fire1")) {
-            FireWeapon();
+            // If the player is looking left, instantiate the bullet and set its velocity to move left.
+            if (Input.GetAxis("Horizontal") < 0) {
+                facingDirection = -1;
+            }
+
+            // Set the spawn position of the bullet to be in front of the player.
+            Vector2 spawnPosition = new Vector2(transform.position.x + 0.7f * facingDirection, transform.position.y - 0.2f);
+
+            // Instantiate bullet here, set velocity and then sleep for short time.
+            Projectile bullet = Instantiate(projectileParent, spawnPosition, Quaternion.identity);
+            Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+            if (facingDirection == 1) {
+                rb.velocity = Vector2.right * projectileSpeed;
+            }
+            else {
+                rb.velocity = Vector2.left * projectileSpeed;
+            }
+
+            yield return new WaitForSeconds(fireRate);
         }
     }
 
-    private void FireWeapon() {
-        Instantiate(projectilePrefab, firePoint.position, firePoint.rotation);
-        rb.velocity = new Vector2(0, 0);
+    private void OnEnable() {
+        attack.action.started += Attack_started;
+        attack.action.performed += Attack_performed;
+        attack.action.canceled += Attack_canceled;
+    }
+
+    // Callbacks for the attack action.
+    // Started is called when the button is first pressed.
+    private void Attack_started(InputAction.CallbackContext obj) {
+        firingCoroutine = StartCoroutine(FireCoroutine());
+    }
+
+    // Performed is called every frame the button is held down.
+    private void Attack_performed(InputAction.CallbackContext obj) { }
+
+    // Canceled is called when the button is released.
+    private void Attack_canceled(InputAction.CallbackContext obj) {
+        StopCoroutine(firingCoroutine);
+    }
+
+    private void OnDisable() {
+        attack.action.started -= Attack_started;
+        attack.action.performed -= Attack_performed;
+        attack.action.canceled -= Attack_canceled;
     }
 }
