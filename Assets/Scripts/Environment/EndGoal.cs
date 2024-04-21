5f62f5f68a3aeb2ade2711b32a7829633a46bbae
Ryan Hogan <Ryanoman2011@Hotmail.com>
2024-04-21 22:25:55 +0100

    Temporarily made all the levels a clone of level one and changed the next level function to actually go to the next level.

--- Assets/Scripts/Environment/EndGoal.cs
+++ Assets/Scripts/Environment/EndGoal.cs
@@ -7,9 +7,17 @@ public class EndGoal : MonoBehaviour
 {
     void OnTriggerEnter2D(Collider2D other)
     {
+        // Load the next scene if there is one, otherwise load the main menu.
         if (other.gameObject.CompareTag("Player")) {
-            // Load the main menu (placeholder).
-            SceneManager.LoadScene(0);
+            int nextSceneBuildIndex = SceneManager.GetActiveScene().buildIndex + 1;
+            if (nextSceneBuildIndex < SceneManager.sceneCountInBuildSettings) {
+                Debug.Log("Loading next level.\nScene index: " + nextSceneBuildIndex);
+                SceneManager.LoadScene(nextSceneBuildIndex);
+            }
+            else {
+                Debug.Log("No more levels to load, returning to main menu.\nScene index: 0");
+                SceneManager.LoadScene(0);
+            }
         }
     }
 }
